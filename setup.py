3225018883f194ec333df879107b9156445a3c6f
tomowarkar <3104kawamoto@gmail.com>
2020-05-07 01:25:51 +0900

    docs: モジュール管理

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="cucurbita",
+    version="0.0.1",
+    description="Japanese dependency parsing tool",
+    author="tomowarkar",
+    url="https://tomowarkar.github.io/blog/",
+    packages=["cucurbita"],
+)
