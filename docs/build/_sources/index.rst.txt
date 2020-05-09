fad206520d331706e3cc46b5418421e372bd9740
tomowarkar <3104kawamoto@gmail.com>
2020-05-09 21:24:18 +0900

    add html docs

---
+++ docs/build/_sources/index.rst.txt
@@ -0,0 +1,22 @@
+.. cucurbita documentation master file, created by
+   sphinx-quickstart on Sat May  9 14:00:10 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to cucurbita's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   cucurbita
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,12 @@ author = "tomowarkar"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary"]
+extensions = [
+    "sphinx.ext.todo",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.githubpages",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -45,7 +50,7 @@ language = "ja"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+# exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
